b57ddd79b805ccb5135387cee72854fcab5c1b82
Nishant Goel <nishant94goel@gmail.com>
2016-03-06 15:37:54 +0530

    Server deployed

--- backend/components/StreetLightServer.php
+++ backend/components/StreetLightServer.php
@@ -26,7 +26,7 @@ class StreetLightServer {
     public $server;
 
     public function __construct() {
-        $this->server = new Server('localhost', 5001, false);
+        $this->server = new Server('ec2-52-89-229-139.us-west-2.compute.amazonaws.com', 5005, false);
         $this->server->setMaxClients(100);
         $this->server->setCheckOrigin(false);
         $this->server->setAllowedOrigin('192.168.1.153');
