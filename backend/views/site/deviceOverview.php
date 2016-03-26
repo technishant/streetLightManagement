603246844defd5cf051565c7f29a18102e698e27
Nishant Goel <nishant94goel@gmail.com>
2016-03-26 23:41:26 +0530

    Added device overview tab

--- backend/views/site/deviceOverview.php
+++ backend/views/site/deviceOverview.php
@@ -21,7 +21,6 @@ $this->params['breadcrumbs'][] = $this->title;
             <?=
             GridView::widget([
                 'dataProvider' => $dataProvider,
-                'filterModel' => $searchModel,
                 'layout' => "{items}\n{pager}",
                 'columns' => [
                     ['class' => 'yii\grid\SerialColumn'],
