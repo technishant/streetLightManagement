43b19e3f3756ddb3f5e2da5496c4b102d32646e5
Nishant Goel <nishant94goel@gmail.com>
2016-03-27 10:08:47 +0530

    Issue fixed for the home page

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -60,9 +60,9 @@ LoginAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Electrotech India <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <!--<p class="pull-right"><?= Yii::powered() ?></p>-->
     </div>
 </footer>
 
